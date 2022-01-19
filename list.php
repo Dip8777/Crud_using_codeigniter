1b69197453edb62e48f565a6643f1cee7c94a48e
Dip8777 <85543955+Dip8777@users.noreply.github.com>
2022-01-19 19:08:02 +0530

    Add files via upload

---
+++ list.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>CRUD Application</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/css/bootstrap.min.css'; ?>">
+</head>
+<body>
+	<div class="navbar navbar-dark bg-dark">
+		<div class="container">
+			<a href="#" class="navbar-brand">Crud App</a>
+		</div>
+	</div>
+	<div class="container" style="padding-top: 10px;">
+	<div class="row">
+		<div class="col-md-12">
+			<?php 
+			$success = $this->session->userdata('success');
+			if($success != ""){
+			?>
+			<div class="alert alert-success"><?php echo $success;?></div>
+			<?php 
+			}
+			?>
+			<?php 
+			$failure = $this->session->userdata('failure');
+			if($failure != ""){
+			?>
+			<div class="alert alert-success"><?php echo $failure;?></div>
+			<?php 
+			}
+			?>
+		</div>
+	</div>
+	        <div class="row">
+				<div class="col-md-8">
+					<div class="row">
+					<div class="col-6"><h4>View User</h4></div>
+			<div class="col-6">
+				<a href="<?php echo base_url().'index.php/user/create';?>" class="btn btn-primary">Create</a>
+			</div>
+			</div>
+			<hr>
+					</div>
+				</div>
+			
+		
+		
+		<div class="row">
+			
+			<div class="col-md-8">
+				<table class="table table-striped">
+					<tr>
+						<th>ID</th>
+						<th>Name</th>
+						<th>Email</th>
+						<th width="70">Edit</th>
+						<th width="100">Delete</th>
+
+					</tr>
+           <?php if(!empty($users)){ foreach($users as $user){ ?>
+					<tr>
+						<td><?php echo $user['user_id']?></td>
+						<td><?php echo $user['name']?></td>
+						<td><?php echo $user['email']?></td>
+                        <td>
+							<a href="<?php echo base_url().'index.php/user/edit/'.$user['user_id']?>" class="btn btn-primary">Edit</a>
+						</td>
+						<td>
+							<a href="<?php echo base_url().'index.php/user/delete/'.$user['user_id']?>" class="btn btn-danger">Delete</a>
+						</td>
+					</tr>
+            <?php } } else{ ?>
+				<tr>
+					<td colspan="5">Record not found</td>
+				</tr>
+				<?php } ?>
+				</table>
+			</div>
+		</div>
+		
+	</div>
+</body>
+</html>
